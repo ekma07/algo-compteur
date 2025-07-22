d1486134127087f9d99b58c342ccbc1dcdc49a03
Aymerick EKOUMA OBIANG <aymerickekouma07@gmail.com>
2025-07-22 13:06:00 +0200

    fin exercice

---
+++ node.js
@@ -0,0 +1,17 @@
+serie1 [3,1,7,9]
+serie2 [2,4,1,9,3]
+sortie = 13 (7+2+4)
+
+function somme elementDistincs (tab1,tab2){
+    let somme = 0;
+    const tous = tab1. concat (tab2);
+}
+
+for(let i=0 < tous.length;i++){
+    const element = tous [i];
+    if (tous,indexOf (element)=== tous.lastindexOf(element)){
+        somme+=element;
+    }
+}
+return somme;
+
